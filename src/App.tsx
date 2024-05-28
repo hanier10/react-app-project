f4977e4c826739fd0d8d26ebbfaf51879e17b5eb
Hanier Morales <hsmp16@gmail.com>
2024-05-27 18:01:45 -0600

    renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ import List from "./components/List";
 function App() {
   //codigo jsx es sumamente parecido a HTML -> React.createElement
   const list = ["Goku", "Tanjiro", "Eren"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo " + elemento);
+  };
+
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin elementos para mostrar"
+  );
+
   return (
     <>
       <Card>
         <CardBody title="Hola mundo" text="Este es el texto" />
-        <List data={list} />
+        {contenido}
+        <List data={list} onSelect={handleSelect} />
       </Card>
     </>
   );
