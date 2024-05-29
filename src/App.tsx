b6392df10866eb14662a5d192620355f5e13b6e2
Hanier Morales <hsmp16@gmail.com>
2024-05-29 15:35:17 -0600

    avance con boton de carga

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
   //codigo jsx es sumamente parecido a HTML -> React.createElement
   const list: string[] = ["Goku", "Tanjiro", "Eren"];
 
@@ -9,13 +14,17 @@ function App() {
     console.log("imprimiendo " + elemento);
   };
 
-  
-
   return (
-    <Card>
-      <CardBody title="Hola mundo" text="Este es el texto" />
-      {list.length !== 0 && <List data={list} onSelect={handleSelect} />}
-    </Card>
+    <>
+      <Card>
+        <CardBody title="Hola mundo" text="Este es el texto" />
+        {list.length !== 0 && <List data={list} onSelect={handleSelect} />}
+
+        <Button isLoading={isLoading} onClick={handleClick}>
+          Hola mundo
+        </Button>
+      </Card>
+    </>
   );
 }
 
